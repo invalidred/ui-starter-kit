30c1c4bc928e891d998628bdaa27c1086318e23d
invalidred <invalidred@gmail.com>
2019-12-24 10:56:14 -0500

    refactor(tslint to eslint): replacing tslint with eslint
    We are replacing ts-lint with eslint because that's the recommended approach going into future as
    typescript will be using eslint and babel in the future.

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react-hooks'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier/@typescript-eslint',
+    "plugin:react/recommended"
+  ],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react/display-name': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/await-thenable': 'off'
+  },
+  settings: {
+    react: { version: 'detect' }
+  }
+}
